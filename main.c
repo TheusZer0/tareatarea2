3bebb9174ae5115d32c48ecf66aafa75da874acd
Quetzal <robert.parra@sansano.usm.cl>
2020-08-01 17:15:11 -0400

    creacion del data.txt arreglado, ya que no hay que crear archivos diferentes, solo se debe crear un archivo una sola vez

--- main.c
+++ main.c
@@ -125,6 +125,7 @@ void mainFunction(int n,int x){
     //  printf("El tiempo que demoro el insertSort es: %f \n",tiempoInsertSort);
     gettimeofday(&start, NULL);
     heapSortFinal=heapsort(heapSort);
+    printf("%d",countNodos(insertSort));
 //    printf(" %d ",countNodos(heapSortFinal));
     gettimeofday(&end, NULL);
     tiempoHeapSort = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000.0;
